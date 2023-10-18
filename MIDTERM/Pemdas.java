8aa48699bae5f93d751da132b15ad7013f8a902c
May Ann Dura <mayannrebucasdura@gmail.com>
2023-10-18 20:23:06 +0800

    Create Pemdas.java

---
+++ MIDTERM/Pemdas.java
@@ -0,0 +1,8 @@
+class Pemdas {
+    public static void main(String[] args) {
+
+        double result = (4 + 8 + 4.5) * 2 - 6 / 9;
+        System.out.println("Value of result is " + result);
+
+    }
+}
