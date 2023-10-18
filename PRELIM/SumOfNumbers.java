aa0bd621f398d68c01084bc4244b8ff9d428c0c6
May Ann Dura <mayannrebucasdura@gmail.com>
2023-10-18 20:04:07 +0800

    Delete PRELIM directory

--- PRELIM/SumOfNumbers.java
+++
@@ -1,9 +0,0 @@
-class SumNum {
-    
-    public static void main(String[] args) {
-        
-        int X = 44, Y = 59, Sum=X+Y; 
-                
-        System.out.println("Sum is " + Sum);
-    }
-}
