91e48e4e890f1f402b6919058540538713ba9442
May Ann Dura <mayannrebucasdura@gmail.com>
2023-10-18 20:17:57 +0800

    Create SumNum.java

---
+++ PRELIM/SumNum.java
@@ -0,0 +1,9 @@
+class SumNum {
+    
+    public static void main(String[] args) {
+        
+        int X = 44, Y = 59, Sum=X+Y; 
+                
+        System.out.println("Sum is " + Sum);
+    }
+}
