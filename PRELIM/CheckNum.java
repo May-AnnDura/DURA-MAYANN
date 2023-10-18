95f201eab073872b3feecc9a2db6e2a03fd5b76f
May Ann Dura <mayannrebucasdura@gmail.com>
2023-10-18 20:00:14 +0800

    Add files via upload

---
+++ PRELIM/CheckNum.java
@@ -0,0 +1,35 @@
+class CheckNum
+ {
+      public static void main(String[] args) {
+           int Number = 33;
+
+
+     //checking whether the number is even or odd
+     if (Number % 2 == 0)
+              System.out.println(Number + " is Even");
+     else
+              System.out.println(Number + " is odd");
+      }
+ }
+ 
+ 
+ /*
+ import java.util.Scanner;
+ 
+ class CheckNum
+ {
+      public static void main(String[] args) {
+           int num;
+           Scanner userInput = new Scanner(System.in);
+           System.out.println("Enter a number:");
+           num = userInput.nextInt(); // Read user input
+
+
+     //checking whether the inputted number is even or odd
+     if (num % 2 == 0)
+              System.out.println(num + " is Even");
+     else
+              System.out.println(num + " is odd");
+      }
+ }*/
+ 
